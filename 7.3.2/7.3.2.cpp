a15e1745cc121c20ca90d6f60a8f6aa78133c935
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2023-02-06 00:31:48 +0300

    Update

--- 7.3.2/7.3.2.cpp
+++ 7.3.2/7.3.2.cpp
@@ -8,44 +8,40 @@
 #include <vector>
 #include <random>
 
-
-//template< class InputIt, class UnaryFunction >
-//UnaryFunction for_each(InputIt first, InputIt last, UnaryFunction f);
-
-template<typename Iterator, typename T, class UnaryFunction>
-T parallel_for_each(Iterator first, Iterator last, UnaryFunction f)
+template<typename Iterator, class UnaryFunction>
+void parallel_for_each(Iterator first, Iterator last, UnaryFunction f)
 {
 	auto length = std::distance(first, last);
 	auto max_chunk_size = 25;
 	if (length <= max_chunk_size)
 	{
-		return std::for_each(first, last, f);
+		std::for_each(first, last, f);
 	}
 	else
 	{
 		Iterator mid_point = first;
 		std::advance(mid_point, length / 2);
-		std::future<T> first_half_result = std::async(std::launch::async, parallel_for_each<Iterator, T>, first, mid_point, f);
-		std::future<T> second_half_result = std::async(std::launch::async, parallel_for_each<Iterator, T>, mid_point, last, f);
-		
-		return first_half_result.get() + second_half_result.get();
+		std::future<void> first_half_result = std::async(std::launch::async, parallel_for_each<Iterator, UnaryFunction >, first, mid_point, f);		
+		parallel_for_each(mid_point, last, f);
+		first_half_result.get();
 	}
 }
 
+
 int main()
 {
-	std::vector<int> V1(1000);
+	std::vector<int> V(60);
+	generate(V.begin(), V.end(), []() {return rand() % 10; });
+	std::cout << "Vector\n";
+	for(int j = 0; j < V.size(); j++) {
+		std::cout << V[j] << " ";
+	}std::cout << "\n\n\n";
 
-	generate(V1.begin(), V1.end(), []() {return rand() % 10; });
-
-	
-
-	auto R2 = parallel_for_each(V1.begin(), V1.end(), [](int i) {
+	std::cout << "parallel for_each:\n";
+	parallel_for_each(V.begin(), V.end(), [](int i) {
 		if (i % 2 == 1)std::cout << i * 3 << " ";
 		else std::cout << i << " ";
-	});
-
-	
+	}); std::cout << "\n\n\n";
 	
     return 0;
 }
